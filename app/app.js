2f92ce5bc7a7768864b7b4abd0e9b77a4f4e5247
Bryan Hadlock <bhadlock@mindfiregroup.com>
2016-03-04 22:51:39 -0700

    first commit

---
+++ app/app.js
@@ -0,0 +1,58 @@
+﻿(function () {
+	"use strict";
+	var app = angular.module("clientManagement",
+                            ["common.services",
+                             "ui.router",
+							 "ui.mask",
+							 "ui.bootstrap",
+                            "clientResourceMock"]);
+	app.config(["$provide", function ($provide) {
+		$provide.decorator("$exceptionHandler",
+			["$delegate",
+				function ($delegate) {
+					return function (exception, cause) {
+						exception.message = "Error: " + exception.message;
+						$delegate(exception, cause);
+						alert(exception.message);
+					}
+				}
+			])
+	}]);
+	app.config(["$stateProvider",
+		"$urlRouterProvider",
+        function ($stateProvider, $urlRouterProvider) {
+        	$urlRouterProvider.otherwise("/");
+        	$stateProvider
+				.state("home", {
+					url: "/",
+					templateUrl: "app/welcomeView.html"
+				})
+				.state("clientList", {
+					url: "/clients",
+					templateUrl: "app/client/clientListView.html",
+					controller: "ClientListController as vm"
+				})
+				.state("clientEdit", {
+					abstract: true,
+					url: "/clients/edit/:productId",
+					templateUrl: "app/client/clientEditView.html",
+					controller: "ClientEditController as vm",
+					resolve: {
+						clientResource: "clientResource",
+						client: function (clientResource, $stateParams) {
+							var clientId = $stateParams.Id;
+							return clientResource.get({ Id: clientId }).$promise;
+						}
+					}
+				})
+				.state("clientEdit.info", {
+					url: "/info",
+					templateUrl: "app/client/clientEditInfoView.html"
+				})
+				.state("clientEdit.stuff", {
+					url: "/price",
+					templateUrl: "app/client/clientEditStuffView.html"
+				})
+        }]
+	);
+}());
---
+++ common/services/clientResourceMock.js
@@ -0,0 +1,71 @@
+﻿(function () {
+	"use strict";
+
+	var app = angular
+                .module("clientResourceMock",
+                ["ngMockE2E"]);
+
+	app.run(function ($httpBackend) {
+		var clients = [
+           {
+           	"Id": 1,
+           	"firstName": "Milo",
+           	"middleName": "Bryan",
+           	"lastName": "Hadlock",
+           	"birthDate": "October 18, 1975",
+           	"gender": "male",
+           	"homeStreet": "123 Main Street",
+           	"homeCity": "SLC",
+           	"homeState": "Utah",
+           	"homePostalCode": "888888",
+           	"maritalStatus": "Single",
+           	"telephone": "8015555555",
+           	"imageUrl": ""
+           }
+		];
+
+		var clientUrl = "api/clients"
+
+		$httpBackend.whenGET(clientUrl).respond(clients);
+
+		var editingRegex = new RegExp(clientUrl + '/[0-9][0-9]*', '')
+		$httpBackend.whenGET(editingRegex).respond(function (method, url, data) {
+			var client = { "clientId": 0 };
+			var parameters = url.split('/');
+			var length = parameters.length;
+			var id = parameters[length - 1];
+
+			if (id > 0) {
+				for (var i = 0; i < clients.length; i++) {
+					if (clients[i].Id == id) {
+						client = clients[i];
+						break;
+					}
+				}
+			}
+			return [200, client, {}];
+		});
+
+
+		$httpBackend.whenPOST(clientUrl).respond(function (method, url, data) {
+			var client = angular.fromJson(data);
+
+			if (!client.Id) {
+				client.Id = clients[clients.length - 1].Id + 1;
+				clients.push(client);
+			}
+			else {
+				for (var i = 0; i < clients.length; i++) {
+					if (clients[i].clientId == client.Id) {
+						clients[i] = client;
+						break;
+					}
+				}
+			}
+			return [200, client, {}];
+		});
+
+		$httpBackend.whenGET(/app/).passThrough();
+
+	})
+}());
